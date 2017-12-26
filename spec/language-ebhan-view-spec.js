00a48890378ff547e43d35682587fa59b080659a
Evan Young <DocCodes@users.noreply.github.com>
2017-12-26 17:46:58 -0600

    Delete language-ebhan-view-spec.js

--- spec/language-ebhan-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import LanguageEbhanView from '../lib/language-ebhan-view';
-
-describe('LanguageEbhanView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
